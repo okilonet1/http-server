23d00a90cf21c1df48262f4487ca7f1afaa21f2b
James Onyekachi <29162889+okilonet1@users.noreply.github.com>
2023-03-15 18:01:31 +0100

    fixed auth and added proper logging

--- http_server.py
+++ http_server.py
@@ -3,20 +3,49 @@ import http.server
 import socketserver
 import logging
 import socket
-import threading
+import base64
+import time
 
 PORT = 8000
 
 # Define the handler to be used
-handler = http.server.SimpleHTTPRequestHandler
+
+
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_AUTHHEAD(self):
+        self.send_response(401)
+        self.send_header("WWW-Authenticate", 'Basic realm="Auth example"')
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def do_GET(self):
+        auth_header = self.headers.get("Authorization")
+        if not auth_header:
+            self.do_AUTHHEAD()
+            self.wfile.write(b'no auth header received')
+        else:
+            auth_decoded = base64.b64decode(auth_header[6:]).decode()
+            username, password = auth_decoded.split(":")
+            if username == "admin" and password == "admin":
+                # Log the file being downloaded and the time it was downloaded
+                logging.info(
+                    f"{self.path} downloaded at {time.strftime('%Y-%m-%d %H:%M:%S')} from {self.client_address[0]}")
+                http.server.SimpleHTTPRequestHandler.do_GET(self)
+            else:
+                self.do_AUTHHEAD()
+                self.wfile.write(b'not authenticated')
 
 
 def start_server(directory):
     # Change the current working directory to the specified directory
     os.chdir(directory)
 
+    # Set up logging
+    logging.basicConfig(filename="http-server.log", level=logging.INFO,
+                        format="%(asctime)s %(levelname)s: %(message)s")
+
     # Define the address to bind to
-    with socketserver.TCPServer(("", PORT), handler) as httpd:
+    with socketserver.TCPServer(("", PORT), CustomHTTPRequestHandler) as httpd:
         # Get the IP address of the machine running the server
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
@@ -25,19 +54,7 @@ def start_server(directory):
 
         print("Serving at URL http://" + ip_address + ":" + str(PORT))
         logging.info("Serving at URL http://" + ip_address + ":" + str(PORT))
-
-        # Start the server in a separate thread
-        server_thread = threading.Thread(target=httpd.serve_forever)
-        server_thread.daemon = True
-        server_thread.start()
-
-        # Wait for keyboard interrupt to stop the server
-        try:
-            while True:
-                pass
-        except KeyboardInterrupt:
-            httpd.shutdown()
-            server_thread.join()
+        httpd.serve_forever()
 
 
 if __name__ == '__main__':
