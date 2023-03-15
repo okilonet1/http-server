6bce43a8f125c0df7cf31991576f7b1e793e75c0
James Onyekachi <29162889+okilonet1@users.noreply.github.com>
2023-03-15 18:40:34 +0100

    added config file and multiple users

--- http_server.py
+++ http_server.py
@@ -3,15 +3,39 @@ import http.server
 import socketserver
 import logging
 import socket
+import configparser
 import base64
-import time
 
 PORT = 8000
+
+# Get the directory of the Python file
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Read the usernames and passwords from the config file
+# config = configparser.ConfigParser()
+# config.read(os.path.join(dir_path, 'server.config'))
+# users = dict(config['users'])
+# print(users)
+users = {}
+
+with open('server.config', 'r') as f:
+    for line in f:
+        line = line.strip()
+        if line:
+            username, password = line.split(':')
+            users[username] = password
+
+print(users)
 
 # Define the handler to be used
 
 
-class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+class AuthHandler(http.server.SimpleHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
     def do_AUTHHEAD(self):
         self.send_response(401)
         self.send_header("WWW-Authenticate", 'Basic realm="Auth example"')
@@ -19,33 +43,33 @@ class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        auth_header = self.headers.get("Authorization")
-        if not auth_header:
+        if self.headers.get("Authorization") is None:
             self.do_AUTHHEAD()
             self.wfile.write(b'no auth header received')
         else:
-            auth_decoded = base64.b64decode(auth_header[6:]).decode()
-            username, password = auth_decoded.split(":")
-            if username == "admin" and password == "admin":
+            auth = self.headers.get("Authorization").split()[1]
+            auth = base64.b64decode(auth).decode()
+            username, password = auth.split(":")
+            if username in users and users[username] == password:
                 # Log the file being downloaded and the time it was downloaded
                 logging.info(
-                    f"{self.path} downloaded at {time.strftime('%Y-%m-%d %H:%M:%S')} from {self.client_address[0]}")
+                    f"{self.path} accessed by {self.client_address[0]} at {self.log_date_time_string()}")
                 http.server.SimpleHTTPRequestHandler.do_GET(self)
             else:
                 self.do_AUTHHEAD()
                 self.wfile.write(b'not authenticated')
 
 
+# Define the handler to be used
+handler = AuthHandler
+
+
 def start_server(directory):
     # Change the current working directory to the specified directory
     os.chdir(directory)
 
-    # Set up logging
-    logging.basicConfig(filename="http-server.log", level=logging.INFO,
-                        format="%(asctime)s %(levelname)s: %(message)s")
-
     # Define the address to bind to
-    with socketserver.TCPServer(("", PORT), CustomHTTPRequestHandler) as httpd:
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
         # Get the IP address of the machine running the server
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
